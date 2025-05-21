ad763d8d2a8e7c67c1a1c394cb0149bfab159e55
sanjiv-65 <mrsanjiv105@gmail.com>
2025-05-21 11:54:23 +0530

    calc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all component files
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
